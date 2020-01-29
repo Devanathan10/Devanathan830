85e5beb536ca1c6ff686a202dbdd4abc94a7bc55
devanathan10 <devaanathan.j@gmail.com>
2020-01-28 23:59:41 -0800

    code

---
+++ src/org/java/Pgm.java
@@ -0,0 +1,13 @@
+package org.java;
+
+public class Pgm {
+
+	public void javac()
+	{
+		System.out.println("jva");
+	}
+		public static void main(String[] args) {
+			Pgm a =new Pgm();
+			a.javac();
+		} 
+	}
